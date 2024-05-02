6279ec1c5c534373f120f075f8825ad3c54a3fad
jteo0 <jjtheodora@gmail.com>
2024-05-02 22:07:17 +0700

    Frontend finished.

--- marmood/urls.py
+++ marmood/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard', include('dashboard.urls')),
-    path('playlist', include('playlist.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('playlist/', include('playlist.urls')),
 ]
