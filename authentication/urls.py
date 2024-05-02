8d7c9dad337005d67c63c87079f33794307ee129
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-05-02 20:24:31 +0700

    change colour pallete and add register html

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from authentication.views import login_page
+from authentication.views import login_page, show_register, show_register_label, show_register_pengguna
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('', login_page, name='login_page'),
+    path('register', show_register, name='show_register'),
+    path('register-pengguna', show_register_pengguna, name='show_register_pengguna'),
+    path('register-label', show_register_label, name='show_register_label'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def login_page(request):
     return render(request, "login.html")
+
+def show_register(request):
+    return render(request, "register.html")
+
+def show_register_pengguna(request):
+    return render(request, "registerPengguna.html")
+
+def show_register_label(request):
+    return render(request, "registerLabel.html")
